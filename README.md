fd3b8d56109e680d73bc04a2dae69435ed7a48fe
flyrain <flyrain000@gmail.com>
2014-03-07 14:25:44 -0600

    * README.md:

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Installation
 
     sudo cp xmonad.desktop /usr/share/xsessions/
 
-    sudo cp custom_xmonad_badge.png /usr/share/unity_greeter
+    sudo cp custom_xmonad_badge.png /usr/share/unity-greeter/
 
 
 Dependent packages install
--- xmonad.hs
+++ xmonad.hs
@@ -85,7 +85,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "synapse")
+    , ((modm,               xK_p     ), spawn "dmenu")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")      
